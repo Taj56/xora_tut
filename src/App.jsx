4398a8187175e3b62751757e9e9b6f01860793ce
Tajay <64923628+Taj56@users.noreply.github.com>
2024-10-22 13:39:38 -0500

    add header
    install react scroll
    install clsx
    style header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
+import Header from "./sections/Header"
 
 
 function App() {
 
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </>
+    <main className="overflow-hidden">
+      <Header />
+    </main>
   )
 }
 
